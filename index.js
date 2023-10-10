3a77a15460ebcd5ab56f88dca7287a0a21088a55
aquil <aquilsaifi987>
2023-10-10 20:37:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// console.log("Welcome");
+
+// console.log(2+4)
+
+//  var , let , const
+
+// console.log("welcome " + "india");
+
+// console.log(2 + "3");
+// console.log("4"+ 3);
+
+// let a;
+// let b = 10;
+// if(a == b) {
+//   console.log(typeof(a));
+//   console.log(typeof(b));
+// }
+
+
+// console.log(a);
+// var a = 10;
+// console.log(a);
+
+
+// let obj = {
+//  name: "xyz",
+//  age: 21,
+//  address: "okhla"
+
+// }
+
+
+// console.log(typeof(obj));
+
+
+let a = function sayHello() {
+    console.log("hello");
+}
+
+let b = a();
+console.log(b);
+
+
+
