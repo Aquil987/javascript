982ab2c11e4f2d4f71ed4927d6ef63de0080b7b7
aquil <aquilsaifi987>
2023-10-18 13:44:09 +0530

    practice Questions

--- JS/conditionalStatement.js
+++ JS/conditionalStatement.js
@@ -2,7 +2,7 @@
 // Syntax ox if statement
 // if(condition)
 // {  }
-
+let x=4;
 if(x>0){
     console.log("Positive number");
 }
@@ -29,3 +29,35 @@ else
 {
     console.log("Non Positive Number.");
 }
+
+
+//Program for Odd and even.
+
+let num =5;
+if(num%2)  // 5%2=1. hence 1 is true for if statement, if will be execute. 
+    console.log("Odd");
+
+else
+    console.log("Even");
+
+num % 2 ? console.log("Odd") : console.log("Even");
+
+//Program to find a number is divisible by 5?
+
+let numb = 1331;
+
+numb % 5 ? console.log("Non Divisible by 5") : console.log("Divisible by 5");
+
+//Program to get an last digit of a number?
+
+let z = 12345;
+z=z%10;
+console.log(z);
+
+let val=1234;
+res=val/100;
+console.log(res);
+
+//program to check a number is divisible by 2 and 3.
+let a1=6;
+a1%2==0 && a1%3==0 ? console.log("divisible") :console.log("Non  divisible");;
---
+++ JS/switchStatement.js
@@ -0,0 +1,20 @@
+
+//Switch Statement
+
+console.log(`What is the capital of India?`);
+console.log(`a: Delhi b: Mumbai \nc: Chennai d: Kolkata`);
+
+let Grade = 'A';
+
+switch (Grade) {
+    case 'A': console.log("good");
+        
+        break;
+        case 'C': console.log("Bad");
+        
+        break;
+    default:
+        console.log('Not Found');
+        break;
+}
+// console.log();
